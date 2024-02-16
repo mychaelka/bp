3ed9079c0429361759aaa9d09e1ef7ec8ecd2c0d
Michaela Kecskésová <x460690@fi.muni.cz>
2024-02-16 19:24:22 +0100

    Fix estimation errors in MAR and nonignorable nonresponse estimators and create new clean class

--- r_playground.R
+++ r_playground.R
@@ -26,3 +26,33 @@ output=treatselDML(y,d,x,s,z)
 cat("ATE: ",round(c(output$effect),3),", standard error: ",
      round(c(output$se),3), ", p-value: ",round(c(output$pval),3))
  output$ntrimmed
+
+result <- c()
+  
+for (i in 1:100) {
+  n=2000                            # sample size
+  p=100                             # number of covariates
+  s=2                               # number of covariates that are confounders
+  sigma=matrix(c(1,0.5,0.5,1),2,2)
+  e=(2*rmvnorm(n,rep(0,2),sigma))
+  x=matrix(rnorm(n*p),ncol=p)       # covariate matrix
+  beta=c(rep(0.25,s), rep(0,p-s))   # coefficients determining degree of confounding
+  d=(x%*%beta+rnorm(n)>0)*1         # treatment equation
+  z=rnorm(n)
+  s=(x%*%beta+0.25*d+z+e[,1]>0)*1   # selection equation
+  y=x%*%beta+0.5*d+e[,2]            # outcome equation
+  y[s==0]=0
+  
+  output=treatselDML(y,d,x,s,z)
+  result <- cbind(result, output$effect)
+}
+
+library(dplyr)
+library(ggplot2)
+matrix(result) %>% as.data.frame() %>% ggplot() + 
+  geom_histogram(aes(x=V1), bins = 10, binwidth = 0.05)
+
+summary(matrix(result))
+
+
+
--- simulation_samplesel.py
+++ simulation_samplesel.py
@@ -2,12 +2,13 @@
 import numpy as np
 import doubleml as dml
 from doubleml.datasets import make_pliv_CHS2015
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.base import clone
 from doubleml import DoubleMLData
 from doubleml.double_ml_samplesel import DoubleMLSS
+from doubleml.double_ml_selection import DoubleMLS
 
-from sklearn.linear_model import LassoCV
+from sklearn.linear_model import LassoCV, Lasso, LogisticRegressionCV
 
 from doubleml.double_ml import DoubleML
 from doubleml.double_ml_data import DoubleMLData
@@ -15,7 +16,9 @@ from doubleml.double_ml_data import DoubleMLData
 from doubleml._utils import _dml_cv_predict, _dml_tune
 from doubleml._utils_checks import _check_score, _check_finite_predictions, _check_is_propensity
 
-#np.random.seed(123)
+import matplotlib.pyplot as plt
+
+np.random.seed(3146)
 
 n = 2000  # sample size
 p = 100  # number of covariates
@@ -35,34 +38,81 @@ y[s == 0] = 0  # Setting values to 0 based on the selection equation
 ##### MISSING AT RANDOM ##### - RMSE'S ARE VERY LARGE, LOOK INTO THAT!!!! 
 ## Creating the DoubleMLData object
 simul_data = DoubleMLData.from_arrays(X, y, d, z=None, t=s)
-#print(simul_data)
 
 learner = LassoCV()
-learner_class = RandomForestClassifier()
+learner_class = LogisticRegressionCV()
 ml_mu_sim = clone(learner)
 ml_pi_sim = clone(learner_class)
 ml_p_sim = clone(learner_class)
 
-obj_dml_sim = DoubleMLSS(simul_data, ml_mu_sim, ml_pi_sim, ml_p_sim)
-obj_dml_sim.fit()
+obj_dml_sim = DoubleMLS(simul_data, ml_mu_sim, ml_pi_sim, ml_p_sim)
+print(obj_dml_sim.fit().summary)
 #obj_dml_sim.sensitivity_analysis()
-print(obj_dml_sim)
+#print(obj_dml_sim)
 #obj_dml_sim.sensitivity_plot()
 
 
 ####### NONIGNORABLE NONRESPONSE #######
 ## Creating the DoubleMLData object
-simul_data = DoubleMLData.from_arrays(X, y, d, z=z, t=s)
+#simul_data = DoubleMLData.from_arrays(X, y, d, z=z, t=s)
 #print(simul_data)
 
-learner = LassoCV()
-learner_class = RandomForestClassifier()
-ml_mu_sim = clone(learner)
-ml_pi_sim = clone(learner_class)
-ml_p_sim = clone(learner_class)
+#learner = LassoCV()
+#learner_class = RandomForestClassifier()
+#ml_mu_sim = clone(learner)
+#ml_pi_sim = clone(learner_class)
+#ml_p_sim = clone(learner_class)
 
-obj_dml_sim = DoubleMLSS(simul_data, ml_mu_sim, ml_pi_sim, ml_p_sim, score='nonignorable_nonresponse')
-obj_dml_sim.fit()
+#obj_dml_sim = DoubleMLS(simul_data, ml_mu_sim, ml_pi_sim, ml_p_sim, score='nonignorable')
+#obj_dml_sim.fit()
 #obj_dml_sim.sensitivity_analysis()
-print(obj_dml_sim)
+#print(obj_dml_sim)
 #obj_dml_sim.sensitivity_plot()
+
+
+####### SEQUENTIAL CONDITIONAL INDEPENDENCE #######
+### TODO
+
+def simul_function():
+    n = 2000  # sample size
+    p = 100  # number of covariates
+    s = 2  # number of covariates that are confounders
+    sigma = np.array([[1, 0.5], [0.5, 1]])
+    e = np.random.multivariate_normal(mean=[0, 0], cov=sigma, size=n).T
+    X = np.random.randn(n, p)  # covariate matrix
+    beta = np.hstack((np.repeat(0.25, s), np.repeat(0, p - s)))  # Coefficients determining the degree of confounding
+    d = np.where(np.dot(X, beta) + np.random.randn(n) > 0, 1, 0)  # Treatment equation
+    z = np.random.randn(n)
+    s = np.where(np.dot(X, beta) + 0.25 * d + z + e[0] > 0, 1, 0)  # Selection equation
+    y = np.dot(X, beta) + 0.5 * d + e[1]  # Outcome equation
+    y[s == 0] = 0
+
+    simul_data = DoubleMLData.from_arrays(X, y, d, z=None, t=s)
+
+    learner = LassoCV()
+    learner_class = LogisticRegressionCV()
+    ml_mu_sim = clone(learner)
+    ml_pi_sim = clone(learner_class)
+    ml_p_sim = clone(learner_class)
+
+    obj_dml_sim = DoubleMLS(simul_data, ml_mu_sim, ml_pi_sim, ml_p_sim)
+    
+    obj_dml_sim.fit()
+
+    return obj_dml_sim.all_coef[0][0]
+
+params = []
+
+# for i in range(500):
+#     params.append(simul_function())
+#     print(i)
+
+# q25, q75 = np.percentile(params, [25, 75])
+# bin_width = 2 * (q75 - q25) * len(params) ** (-1/3)
+# bins = round((max(params) - min(params)) / bin_width)
+
+# print("Freedman-Diaconis number of bins:", bins)
+# plt.hist(params, density=True, bins=bins)
+# plt.xlabel('ATE estimates')
+# plt.ylabel('Count')
+# plt.show()
